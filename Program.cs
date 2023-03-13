ed0125a0988cac9d94f22026c62d1e7b0f45b801
ИИП ТТ <kab31lab@gmail.com>
2023-03-13 09:42:50 +0200

    zadanie 2

--- Program.cs
+++ Program.cs
@@ -6,77 +6,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //int[] array = new int[12]; 
 
-            //Random rnd = new Random(); 
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    array[i] = rnd.Next(163, 191); 
-            //}
-
-            //Console.WriteLine("Рост людей:");
-            //foreach (int height in array)
-            //{
-            //    Console.WriteLine(height);
-            //}
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    Console.WriteLine($"array[{i}] = {array[i]}");
-            //}
-
-
-            //Random rnd = new Random();
-            //double[] array = new double[10];
-
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    array[i] = rnd.NextDouble() * (50 - (-50)) - 50; 
-            //}
-
-            //Console.WriteLine("Первый элемент: " + array[0]);
-            //Console.WriteLine("Последний элемент: " + array[array.Length - 1]);
-
-
-            //int[] array = new int[] { 74, 0, 50, 46, 34, 46, 0, 13 };
-
-            //foreach (int element in array)
-            //{
-            //    Console.WriteLine(element);
-            //}
-
-
-            /* int[] array = new int[] { 74, 0, 50, 46, 34, 46, 0, 13 }; */              // Первый способ заданных чисел внутри массива
-                                                                                         // int[] array = { 74, 0, 50, 46, 34, 46, 0, 13 };                      Второй способ заданных чисел внутри массива
-
-            //foreach (int element in array)
-            //{
-            //    Console.WriteLine(element);
-            //    int result = element * element;
-            //    Console.WriteLine(result);
-            //}
-
-
-            //Задача 1.  Заполнить одномерный массив с клавиатуры шестью целыми числами. Вывести одномерный массив в обратном порядке.
-
-            int[] array = new int[6];
-
-            for (int i = 0; i < array.Length; i++) // Последовательно
+            int[] array = new int[31];
+            Random random = new Random();
+            for(int i=0; i < array.Length; i++)
             {
-                Console.Write($"array[{i}] = ");
-                array[i] = int.Parse(Console.ReadLine());
-            }
-
-            for (int i = array.Length-1; i >= 0; i--) //    Обратно
-                {
+                array[i] = random.Next(5,60);
                 Console.WriteLine($"array[{i}] = {array[i]}");
-
             }
 
-            Console.Read();
-            //3
+            int summ = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                summ += array[i];
+            }
 
+            Console.WriteLine($"Общее кол-во осадков = {summ}");
         }
     }
 }
